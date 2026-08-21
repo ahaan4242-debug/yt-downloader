9486ea33190e90eaa5841a26cd981f3894fb45b6
ahaan4242-debug <ahaan4242@gmail.com>
2026-08-21 12:35:51 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+[0;1;39m4 - Tayyab (1000)[0m
+[0;94m  Since:[0m Wed 2026-08-19 10:08:26 PKT; 5min ago
+[0;94m  State:[0m active
+[0;94m Leader:[0m 31534 (ddm)
+[0;94m   Seat:[0m seat0; vc4
+[0;94m    TTY:[0m tty4
+[0;94m Remote:[0m no
+[0;94mService:[0m ddm
+[0;94m   Type:[0m wayland
+[0;94m  Class:[0m user
+[0;94mDesktop:[0m Treeland
+[0;94m   Idle:[0m no
+[0;94m   Unit:[0m session-4.scope
+         ├─[0;38;5;245m31534 /usr/bin/ddm[0m
+         ├─[0;38;5;245m31541 /usr/bin/gnome-keyring-daemon --daemonize --login[0m
+         └─[0;38;5;245m31545 /usr/bin/dde-session[0m
+
+Aug 19 10:08:26 Tayyab dde-session[31545]: environments manager init
+Aug 19 10:08:26 Tayyab dde-session[31545]: [0;38;5;245m[0;1;31m[0;38;5;245mclear env, result:  true[0m
+Aug 19 10:08:26 Tayyab dde-session[31545]: environments manager init finished
+Aug 19 10:08:26 Tayyab dde-session[31545]: start dm service: "dde-session.target"
+Aug 19 10:08:26 Tayyab dde-session[31545]: success to start systemd unit: "dde-session.target" , job path: "/org/freedesktop/systemd1/job/2249"
+Aug 19 10:08:26 Tayyab dde-session[31545]: leader pipe thread id:  0x7f2a573226c0 , pid:  31545
